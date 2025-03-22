137cddd9da2b90e181d74e7b76bc97e93deb5530
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-03-22 10:41:18 +0900

    update

--- create-tag-rulesets/list_repos.sh
+++ create-tag-rulesets/list_repos.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+mkdir -p owners
+
 while read -r owner; do
     gh repo list "$owner" --no-archived --visibility public -L 1000 --json nameWithOwner > "owners/$owner.json"
 done < owners.txt
