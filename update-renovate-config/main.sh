fc95cab1267eefdefbb19da63ba802ecd511c977
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-04-07 10:28:56 +0900

    update

--- update-renovate-config/main.sh
+++ update-renovate-config/main.sh
@@ -5,5 +5,5 @@ set -eu
 # 'github>suzuki-shunsuke/renovate-config#2.3.0',
 # 'github>suzuki-shunsuke/renovate-config:nolimit#2.3.0',
 
-file=renovate.json5
-set -i 's|github>suzuki-shunsuke/renovate-config(:[^#]+)?(#\d+.\d+.\d+)?|github>suzuki-shunsuke/renovate-config\1#3.2.0' "$file"
+file=renovate.json
+sed -i -E 's|github>suzuki-shunsuke/renovate-config(\:[^#]+)?(#[0-9]+\.[0-9]+\.[0-9]+)?|github>suzuki-shunsuke/renovate-config\1#3.2.0|' "$file"
