f159a85e50bf14707dd8bcddacc9c59084cb9e4b
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-06-14 21:14:22 +0900

    update

--- replace-actionlint/merge.sh
+++ replace-actionlint/merge.sh
@@ -2,16 +2,7 @@
 
 set -eu
 
-org=$1
-
 export GITHUB_TOKEN=$(gh auth token)
 
-opt=-O
-if [ "$org" = suzuki-shunsuke ]; then
-	opt=-U
-fi
-
 multi-gitter merge \
-	-B ci-add-actionlint-workflow \
-	--skip-forks \
-	"$opt" "$org"
+	--config config.yaml
