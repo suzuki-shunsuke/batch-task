0efdcefb7d8baa7372f2710fa7cdc2781fddbe7d
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-07-06 08:14:13 +0900

    update

--- fix-status-check-if/main.sh
+++ fix-status-check-if/main.sh
@@ -2,5 +2,5 @@
 
 set -eu
 
-git grep -l -E " +if: failure()" .github/workflows | xargs -n 1 sed -i -E 's/failure\(\)/always() \&\& (contains(needs.*.result, 'failure') || contains(needs.*.result, 'cancelled'))/'
+git grep -l -E " +if: failure()" .github/workflows | xargs -n 1 sed -i -E "s/failure\(\)/always() \&\& (contains(needs.*.result, 'failure') || contains(needs.*.result, 'cancelled'))/"
 actionlint
